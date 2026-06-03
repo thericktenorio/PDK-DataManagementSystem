ac41ec875fcad8f51eca66e7bbad93a466157414
RICK TENORIO <tenorio@TENORIOs-Mac-mini.local>
2026-06-03 14:25:27 -0700

    Fix admin 500: require organization on InternalUser form

--- pdk_crm/accounts/admin.py
+++ pdk_crm/accounts/admin.py
@@ -13,7 +13,7 @@ class InternalUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'role')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'role', 'organization')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')}),
         ('Important dates', {'fields': ('last_login', )}),
     )
@@ -21,6 +21,9 @@ class InternalUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name','role', 'password1', 'password2', 'is_staff', 'is_active')
+            'fields': (
+                'email', 'first_name', 'last_name', 'organization', 'role',
+                'password1', 'password2', 'is_staff', 'is_active', 'is_superuser',
+            ),
         }),
     )
